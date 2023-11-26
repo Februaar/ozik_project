7682788b812b2b6780be3b70e1a1123f6de6e2c3
Kyungjin Jung <kyoooungjin@gmail.com>
2023-11-27 01:15:58 +0900

    markup: main, search, product page layout

---
+++ src/pages/Search.jsx
@@ -0,0 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import "../styles/search.scss";
+import { close, cancel } from "../img/index";
+
+function SearchPage() {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <>
+      <div className="search-container">
+        <div className="top">
+          <input className="search-input" />
+          <img src={cancel} className="delete" alt="Cancel" />
+        </div>
+        <div className="content-area">
+          <div className="search-value">
+            <span className="title">최근 검색어</span>
+            <div className="value-container">
+              <span className="value">스타벅스</span>
+              <span className="value">일리</span>
+              <span className="value">네스카페</span>
+              <span className="value">네스카페</span>
+              <span className="value">네스카페</span>
+              <span className="value">홀리몰리 과카몰리</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button className="close-button" onClick={handleGoBack}>
+        <img src={close} className="close" alt="Close" />
+      </button>
+    </>
+  );
+}
+
+export default SearchPage;
