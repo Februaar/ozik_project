ab1b8129fed56880ae99e3789b99c94bee2a7bf6
Kyungjin Jung <kyoooungjin@gmail.com>
2023-11-29 02:05:20 +0900

    build 재진행

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import react from "@vitejs/plugin-react";
-import ViteSassPlugin from "vite-plugin-sass";
+// import ViteSassPlugin from "vite-plugin-sass";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteSassPlugin()],
+  plugins: [react()],
   server: {
     port: 4000,
   },
