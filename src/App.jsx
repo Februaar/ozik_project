f6c6435509bd24da6a3702df882cc14238a0604c
Kyungjin Jung <kyoooungjin@gmail.com>
2024-12-16 18:58:06 +0900

    모든 페이지에서 menuOpen 상태 기반으로 category 컴포넌트 렌더링하도록 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import "./global.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { useMenu } from "./context/MenuContext";
+import "./global.css";
 import Header from "./components/Header";
+import Category from "./components/Category";
 import Hr from "./components/ui/hr";
 import Footer from "./components/Footer";
 import ProductList from "./components/ProductList";
@@ -14,9 +15,12 @@ import MyPage from "./pages/My";
 // import SearchPage from "./pages/Search";
 
 function App() {
+  const { menuOpen } = useMenu();
+
   return (
     <Router>
       <Header />
+      {menuOpen && <Category />}
       <Routes>
         <Route path="/*" element={<MainPage />} />
         <Route path="/signup" element={<SignUpPage />} />
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,17 +1,13 @@
 import "../styles/main.scss";
-import { useMenu } from "../context/MenuContext";
 import Navigation from "../components/Navigation";
 import Carousel from "../components/Carousel";
-import Category from "../components/Category";
 
 export default function MainPage() {
-  const { menuOpen } = useMenu();
 
   return (
     <div className="main-container">
       <Carousel />
       <Navigation />
-      {menuOpen ? <Category /> : ""}
     </div>
   );
 }
